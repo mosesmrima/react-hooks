26426a8e6ac1077b6fd776d9fd5885bce81d2173
mrima <mrimamss@gmail.com>
2023-01-23 10:04:19 +0300

    use reducer to make a stateful component

--- src/hooks/UseReducer.jsx
+++ src/hooks/UseReducer.jsx
@@ -1,8 +1,25 @@
 import {useReducer} from "react";
 
+const reducer = (state, action) => {
+    switch (action.operation) {
+        case "increment":
+            return {count: state.count + 1}
+        case "decrement":
+            return {count: state.count - 1}
+        default:
+            return {count: state.count}
+    }
+}
+
 export const UseReducer = () => {
+    const [state, dispatch] = useReducer(reducer, {count: 0})
+
     return (
         <>
+            <h1>useReducer</h1>
+            <button onClick={()=>dispatch({operation: "increment"})}>+</button>
+            <span>{state.count}</span>
+            <button onClick={()=> dispatch({operation: "decrement"})}>-</button>
         </>
     );
 }

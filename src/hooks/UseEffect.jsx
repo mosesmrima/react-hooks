ec87c9e41f23afeb2d1cf99735c840a3b071a7d6
mrima <mrimamss@gmail.com>
2023-01-23 14:44:13 +0300

    fetch todo on first render only, update on save

--- src/hooks/UseEffect.jsx
+++ src/hooks/UseEffect.jsx
@@ -1,17 +1,40 @@
 import {useEffect, useState} from "react";
 
 export const UseEffect = () => {
-    const [count, setCount] = useState(0)
-    useEffect(() =>{
-        let timer = setTimeout(() => {
-            setCount(count + 1)
-        }, 1000)
-        return () =>  clearTimeout(timer)
+   const [todo, setTodo] = useState("")
+    const [todoList, addTodo] = useState([])
+    useEffect(() => {
+        addTodo(JSON.parse(localStorage.getItem("todoList")))
     }, [])
+    const handleAdd = () => {
+       if(todo && !todoList.includes(todo)) {
+           addTodo(prevTodo => [...prevTodo, todo])
+       }
+    }
+    const handleDone = (el) => {
+      const i = todoList.indexOf(el)
+       const newTodo = todoList.filter( (el, index) => i !== index )
+        addTodo(newTodo)
+    }
     return (
         <>
-            <h1>useEffect</h1>
-            <p>I have re-rendered {count} times</p>
+            <h1>use Effect</h1>
+           <input
+               type={"text"}
+               name={"todo"}
+               value={todo}
+               onChange={() => setTodo(event.target.value)}
+           />
+            <button
+                onClick={handleAdd}
+            >Add todo</button>
+            <div>
+                <h5>My todo list</h5>
+                <ul>
+                    {todoList.map( el => <li key={Math.floor(Math.random() * 100)}>{el} <button onClick={() => handleDone(el)}>Done</button> </li>)}
+                </ul>
+                <button onClick={() => localStorage.setItem("todoList", JSON.stringify(todoList))}>Save Todo</button>
+            </div>
         </>
     );
 }

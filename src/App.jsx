7af18e29ab92019c050a84ff9e97a30c5a40232f
mrima <mrimamss@gmail.com>
2023-01-23 10:03:46 +0300

    import state component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import {UseReducer} from "./hooks/UseReducer.jsx";
+import {UseState} from "./hooks/UseState.jsx";
 
 function App() {
 
   return (
     <div className="App">
-
+        <UseReducer/>
+        <UseState/>
     </div>
   )
 }

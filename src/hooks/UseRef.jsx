fba237b16844519ecac90e5d9c8a80a8802a1ac2
mrima <mrimamss@gmail.com>
2023-01-23 10:33:45 +0300

    using useRef hook

---
+++ src/hooks/UseRef.jsx
@@ -0,0 +1,13 @@
+import {useRef} from "react";;
+
+export const UseRef = () => {
+    const inputRef = useRef()
+    const focusInput = () => inputRef.current.focus()
+    return (
+        <>
+            <h1>useRef</h1>
+            <input type={"text"} ref={inputRef}/>
+            <button onClick={focusInput}>Focus on input</button>
+        </>
+    );
+}
